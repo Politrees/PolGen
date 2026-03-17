3b95f468debbff524e73047985a7ddf5ffb43854
Artyom Bebroy <artyom.vasilenko.99@list.ru>
2026-03-18 00:27:33 +0500

    Небольшой рефакторинг welcome.py

--- gradio_ui/welcome.py
+++ gradio_ui/welcome.py
@@ -2,52 +2,15 @@ import gradio as gr
 
 
 def welcome_tab():
-    gr.HTML(
-        """
-    <center>
-        <h1 style="font-size: 3em;">
-            <b>Добро пожаловать в PolGen</b>
-        </h1>
-    </center>
-    """,
-    )
+    gr.HTML("<center><h1 style='font-size: 3em;'><b>Добро пожаловать в PolGen</b></h1></center>")
     with gr.Row():
         with gr.Column(variant="panel"):
-            gr.HTML(
-                "<center><h2>"
-                "<a href='https://t.me/Politrees2'>"  # Ссылка
-                "Telegram ЛС"  # Имя ссылки
-                "</a></h2></center>",
-            )
-            gr.HTML(
-                "<center><h2>"
-                "<a href='https://vk.com/artem__bebroy'>"  # Ссылка
-                "ВКонтакте (страница)"  # Имя ссылки
-                "</a></h2></center>",
-            )
+            gr.HTML("<center><h2><a href='https://t.me/Politrees2'>Telegram ЛС</a></h2></center>")
+            gr.HTML("<center><h2><a href='https://vk.com/artem__bebroy'>ВКонтакте</a></h2></center>")
         with gr.Column(variant="panel"):
-            gr.HTML(
-                "<center><h2>"
-                "<a href='https://t.me/pol1trees'>"  # Ссылка
-                "Telegram Канал"  # Имя ссылки
-                "</a></h2></center>",
-            )
-            gr.HTML(
-                "<center><h2>"
-                "<a href='https://t.me/+GMTP7hZqY0E4OGRi'>"  # Ссылка
-                "Telegram Чат"  # Имя ссылки
-                "</a></h2></center>",
-            )
+            gr.HTML("<center><h2><a href='https://t.me/pol1trees'>Telegram Канал</a></h2></center>")
+            gr.HTML("<center><h2><a href='https://t.me/+GMTP7hZqY0E4OGRi'>Telegram Чат</a></h2></center>")
+
     with gr.Column(variant="panel"):
-        gr.HTML(
-            "<center><h2>"
-            "<a href='https://www.youtube.com/@Politrees?sub_confirmation=1'>"  # Ссылка
-            "YouTube"  # Имя ссылки
-            "</a></h2></center>",
-        )
-        gr.HTML(
-            "<center><h2>"
-            "<a href='https://github.com/Politrees/PolGen'>"  # Ссылка
-            "GitHub"  # Имя ссылки
-            "</a></h2></center>",
-        )
+        gr.HTML("<center><h2><a href='https://www.youtube.com/@Politrees'>YouTube</a></h2></center>")
+        gr.HTML("<center><h2><a href='https://github.com/Politrees/PolGen'>GitHub</a></h2></center>")
